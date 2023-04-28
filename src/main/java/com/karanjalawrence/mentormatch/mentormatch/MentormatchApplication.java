e5715821b8e4b323983d493302ea2a5f833a45ff
lawrence254 <lawrenslava1@gmail.com>
2023-04-28 13:59:01 +0300

    Create initial structure for the project

---
+++ src/main/java/com/karanjalawrence/mentormatch/mentormatch/MentormatchApplication.java
@@ -0,0 +1,13 @@
+package com.karanjalawrence.mentormatch.mentormatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentormatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentormatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karanjalawrence/mentormatch/mentormatch/MentormatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karanjalawrence.mentormatch.mentormatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentormatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
