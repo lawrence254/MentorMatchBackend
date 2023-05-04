db634f0a6d27c16399811767a5f1fb42d3107b6c
lawrence254 <lawrenslava1@gmail.com>
2023-05-04 17:48:34 +0300

    Hide password from API response

--- src/main/java/com/karanjalawrence/mentormatch/mentormatch/Domain/User.java
+++ src/main/java/com/karanjalawrence/mentormatch/mentormatch/Domain/User.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,6 +30,7 @@ public class User implements UserDetails {
     private UUID id;
     @Column(unique = true, name = "email_address")
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private Boolean enabled;
     private Roles role;
