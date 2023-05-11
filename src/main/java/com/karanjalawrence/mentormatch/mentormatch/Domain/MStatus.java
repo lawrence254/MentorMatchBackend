c6ee5d1b9c2918af877ecab1e992d44e48c20078
lawrence254 <lawrenslava1@gmail.com>
2023-05-11 14:56:45 +0300

    Create Domain and Enums for Meetings

---
+++ src/main/java/com/karanjalawrence/mentormatch/mentormatch/Domain/MStatus.java
@@ -0,0 +1,9 @@
+package com.karanjalawrence.mentormatch.mentormatch.Domain;
+
+public enum MStatus {
+    Upcoming, 
+    Done, 
+    Rescheduled, 
+    Cancelled,
+    NoShow
+}
---
+++ src/main/java/com/karanjalawrence/mentormatch/mentormatch/Domain/Meeting.java
@@ -0,0 +1,42 @@
+package com.karanjalawrence.mentormatch.mentormatch.Domain;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Meeting {
+    @Id
+    @GeneratedValue
+    private UUID meetId;
+    private MeetMethod method;
+    @Column(name = "meeting_link")
+    private String link;
+    @Column(name = "organizer")
+    private UserDetails user;
+    @Column(name = "meet_with")
+    private UserDetails with;
+    @Column(name = "meeting_date")
+    private Date mDate;
+    private MStatus status;
+    @Column(name ="reschedule_date")
+    private Date rescheduleTo;
+    @CreationTimestamp
+    private Date created_at;
+    @UpdateTimestamp
+    private Date updated_at;
+}
